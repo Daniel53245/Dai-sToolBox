818c3775a64e2974217333c75906a633c3b37f01
Dai Boyu <u7241110@anu.edu.au>
2022-12-20 10:52:51 +1100

    implement bubble sort

---
+++ Sortings/sorting.hpp
@@ -0,0 +1,7 @@
+#ifndef sorting
+#define sorting
+    #include <stdlib.h>
+    #include <iostream>
+    void print_array(int input[],int length);
+    void bubble_sort(int input[],int length);
+#endif
